e4071aa7c227322603333c3fefbeef87afde9c40
sergius0301 <sergiu0301>
2015-04-24 21:20:44 +0300

    Added contacts

--- SocialityApp/Controllers/HomeController.cs
+++ SocialityApp/Controllers/HomeController.cs
@@ -8,9 +8,23 @@ namespace SocialityApp.Controllers
 {
     public class HomeController : Controller
     {
+    public class HomeController : Controller
+    {
         public ActionResult Index()
         {
-            ViewBag.Title = "Home Page";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
 
             return View();
         }
